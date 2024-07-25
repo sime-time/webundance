5796866d5388139593713d07a97f29efffaaf78d
Dunn, Simeon <simedunn01@gmail.com>
2024-07-25 09:53:34 -0400

    hero review image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,15 @@
 import { component$ } from "@builder.io/qwik";
 
+import HeroReview from "../assets/hero-review.png?jsx";
+
 export default component$(() => {
   return (
-    <section class="p-4 md:p-5 mx-auto max-w-6xl flex flex-row border">
+    <section class="p-4 md:p-5 mx-auto max-w-6xl flex flex-row gap-4">
       <div>
-        <h1 class="text-6xl">Fast websites designed to turn visitors into customers.</h1>
+        <h1 class="text-6xl font-bold">Fast websites designed to turn visitors into clients.</h1>
       </div>
-      <div class="glass-container">
-        <h2>Glass review here medard 5 stars </h2>
+      <div class="glass-container px-5">
+        <HeroReview style={{ width: '900px' }} />
       </div>
     </section>
   );
