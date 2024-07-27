8909c80ba8b2e9116cdc894ec33055a56555f722
Dunn, Simeon <simedunn01@gmail.com>
2024-07-26 23:33:35 -0400

    navbar yellow logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { component$, useSignal, $ } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
 import { IoMenuSharp, IoCloseSharp } from "@qwikest/icons/ionicons";
-import Logo from "./Logo";
+import Logo from "../assets/logo.svg?jsx";
 
 interface NavItem {
   name: string;
@@ -34,9 +34,15 @@ export default component$(() => {
 
   return (
     <nav class="py-4 px-4 md:py-6 mx-auto max-w-6xl flex flex-row justify-between font-medium text-white items-center">
-      <div class="w-16 h-16">
-        <Link href="/"><Logo /> </Link>
-      </div>
+      <Link href="/">
+        <div class="flex flex-row items-center gap-4 ">
+          <div class="h-16 w-16">
+            <Logo />
+          </div>
+          <span class=" text-lg md:text-2xl text-yellow-300 font-bold tracking-wider">Webundance</span>
+        </div>
+      </Link>
+
       <div class={`md:static absolute md:min-h-fit min-h-[90vh] transition-[left] ${menuLeft.value} md:w-auto w-full flex items-center px-5 top-[12%] bg-slate-800 md:bg-transparent z-20`}>
         <ul class="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-16 text-xl md:text-base">
           {navItems.map((item, index) => (
@@ -47,7 +53,7 @@ export default component$(() => {
         </ul>
       </div>
       <div class="flex items-center gap-6">
-        <button class="focus:ring-offset-3 relative inline-flex h-fit w-fit rounded-lg border border-blue-100/20 bg-blue-200/10 px-6 py-3 text-blue-200 outline-none ring-yellow-300 transition-colors after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-yellow-100 after:bg-opacity-0 after:blur-md after:transition-all after:duration-500 hover:border-yellow-200/40 hover:text-yellow-300 after:hover:bg-opacity-15 focus:ring-2">Contact</button>
+        <button class="focus:ring-offset-3 relative inline-flex h-fit w-fit rounded-lg border border-blue-100/20 bg-blue-200/10 px-4 py-2 text-blue-200 outline-none ring-yellow-300 transition-colors after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-yellow-100 after:bg-opacity-0 after:blur-md after:transition-all after:duration-500 hover:border-yellow-200/40 hover:text-yellow-300 after:hover:bg-opacity-15 focus:ring-2">Contact</button>
         <div class="text-3xl cursor-pointer md:hidden" onClick$={toggleMenu}>{menuIsOpen.value ? <IoCloseSharp /> : <IoMenuSharp />} </div>
       </div>
     </nav>
