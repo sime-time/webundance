b1a2c963e6909412a9923ad477df93e21ce4f880
Dunn, Simeon <simedunn01@gmail.com>
2024-07-27 16:56:42 -0400

    mobile navbar updated dropdown

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,11 +3,11 @@ import { IoStarSolid } from "@qwikest/icons/ionicons";
 
 export default component$(() => {
   return (
-    <section class="px-4 py-5 md:py-12 mx-auto max-w-6xl flex flex-col md:flex-row gap-10">
+    <section class="px-4 py-5 md:py-12 mx-auto max-w-6xl flex flex-col md:flex-row gap-16 md:gap-10">
 
       <div class="flex flex-col gap-9 basis-2/3">
         <h1 class="text-6xl text-wrap leading-tight">Fast websites that turn <b class="italic">clicks into clients.</b></h1>
-        <p class="text-xl leading-normal">We provide copywriting and web development to make your website <br />a lead-converting machine.</p>
+        <p class="text-xl leading-normal">We provide copywriting and web development to make your website a lead-converting machine.</p>
         <div class="flex flex-row items-center gap-8 md:gap-12 ">
           <button class="relative justify-center cursor-pointer px-6 py-3 border bg-yellow-500 border-yellow-400 rounded-lg hover:bg-yellow-400 hover:border-yellow-300">Start my website</button>
           <a href="#pricing" class="focus:ring-offset-3 relative inline-flex h-fit w-fit rounded-lg border border-blue-100/20 bg-blue-200/10 px-6 py-3 text-blue-200 outline-none ring-yellow-300 transition-colors after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-yellow-100 after:bg-opacity-0 after:blur-md after:transition-all after:duration-500 hover:border-yellow-200/40 hover:text-yellow-300 after:hover:bg-opacity-15 focus:ring-2">Pricing</a>
@@ -17,7 +17,7 @@ export default component$(() => {
       <div class="glass-container p-6 md:p-8 flex flex-col justify-between h-full w-full gap-6 basis-1/3">
 
         <div class="flex flex-row justify-between items-center">
-          <img src="/collegecarry.png" alt="College carry logo" width="100" height="50" />
+          <img src="collegecarry.png" alt="College carry logo" width="100" height="50" />
           <div class="flex flex-row text-white text-2xl">
             <IoStarSolid />
             <IoStarSolid />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,43 +19,63 @@ export default component$(() => {
   ];
 
   const menuIsOpen = useSignal(false);
-  const menuLeft = useSignal("left-[-100%]");
+  const showMenuClass = useSignal("");
 
   const toggleMenu = $(() => {
     menuIsOpen.value = !menuIsOpen.value;
 
     if (menuIsOpen.value == true) {
-      menuLeft.value = "left-0";
+      showMenuClass.value = "is-open";
     } else {
-      menuLeft.value = "left-[-100%]";
+      showMenuClass.value = "";
     }
   });
 
 
   return (
-    <nav class="py-4 px-4 md:py-6 mx-auto max-w-6xl flex flex-row justify-between font-medium text-white items-center">
-      <Link href="/">
-        <div class="flex flex-row items-center gap-4 ">
-          <div class="h-16 w-16">
-            <Logo />
-          </div>
-          <span class=" text-lg md:text-2xl text-yellow-300 font-bold tracking-wider">Webundance</span>
-        </div>
-      </Link>
+    <nav class=" mx-auto max-w-6xl font-medium text-white items-center flex flex-col w-full">
+      <div class="py-4 px-4 md:py-6 flex flex-row flex-wrap justify-between w-full">
 
-      <div class={`md:static absolute md:min-h-fit min-h-[90vh] transition-[left] ${menuLeft.value} md:w-auto w-full flex items-center px-5 top-[12%] bg-slate-800 md:bg-transparent z-20`}>
-        <ul class="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-16 text-xl md:text-base">
+        <Link href="/">
+          <div class="flex flex-row items-center gap-2 md:gap-4">
+            <div class="h-16 w-16">
+              <Logo />
+            </div>
+            <span class=" text-lg md:text-2xl text-yellow-300 font-bold tracking-wider">Webundance</span>
+          </div>
+        </Link>
+
+        <div class="hidden sm:flex w-auto static min-h-fit items-center px-5 bg-slate-800 bg-transparent z-20">
+          <ul class="flex flex-row items-center gap-[4vw] text-base">
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <Link href={item.link} class="hover:text-yellow-400 py-4">{item.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div class="flex items-center gap-4">
+          <button class="focus:ring-offset-3 relative hidden sm:inline-flex h-fit w-fit rounded-lg border border-blue-100/20 bg-blue-200/10 px-4 py-2 text-blue-200 outline-none ring-yellow-300 transition-colors after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-yellow-100 after:bg-opacity-0 after:blur-md after:transition-all after:duration-500 hover:border-yellow-200/40 hover:text-yellow-300 after:hover:bg-opacity-15 focus:ring-2">Contact</button>
+          <div class="text-3xl cursor-pointer sm:hidden" onClick$={toggleMenu}>{menuIsOpen.value ? <IoCloseSharp /> : <IoMenuSharp />} </div>
+        </div>
+
+      </div>
+
+      <div class={`sm:hidden w-[100vw] wrapper ${showMenuClass.value}`}>
+        <ul class="flex flex-col text-2xl inside-wrapper">
           {navItems.map((item, index) => (
-            <li key={index}>
-              <Link href={item.link} class="hover:text-yellow-400 py-4">{item.name}</Link>
+            <li key={index} class="border-b py-4 mx-4">
+              <Link href={item.link} class="hover:text-yellow-300">{item.name}</Link>
             </li>
           ))}
+          <li class="py-6 mx-4 w-auto">
+            <button class="focus:ring-offset-3 relative text-center justify-center sm:hidden inline-flex h-fit w-full rounded-lg border border-blue-100/20 bg-blue-200/10 px-4 py-2 text-blue-200 outline-none ring-yellow-300 transition-colors after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-yellow-100 after:bg-opacity-0 after:blur-md after:transition-all after:duration-500 hover:border-yellow-200/40 hover:text-yellow-300 after:hover:bg-opacity-15 focus:ring-2">Contact</button>
+          </li>
+
         </ul>
       </div>
-      <div class="flex items-center gap-6">
-        <button class="focus:ring-offset-3 relative inline-flex h-fit w-fit rounded-lg border border-blue-100/20 bg-blue-200/10 px-4 py-2 text-blue-200 outline-none ring-yellow-300 transition-colors after:absolute after:inset-0 after:-z-10 after:animate-pulse after:rounded-full after:bg-yellow-100 after:bg-opacity-0 after:blur-md after:transition-all after:duration-500 hover:border-yellow-200/40 hover:text-yellow-300 after:hover:bg-opacity-15 focus:ring-2">Contact</button>
-        <div class="text-3xl cursor-pointer md:hidden" onClick$={toggleMenu}>{menuIsOpen.value ? <IoCloseSharp /> : <IoMenuSharp />} </div>
-      </div>
+
     </nav>
   );
 });
