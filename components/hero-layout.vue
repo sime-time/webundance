434cce9b06e40b5f45e9f2f8461175a67758092e
Dunn, Simeon <simedunn01@gmail.com>
2025-06-06 10:12:09 -0400

    75vh for hero section

--- components/hero-layout.vue
+++ components/hero-layout.vue
@@ -1,5 +1,5 @@
 <template>
-	<section class="relative bg-gradient-to-b from-white to-primary-100 flex justify-center items-center h-[80vh]">
+	<section class="relative bg-gradient-to-b from-white to-primary-100 flex justify-center items-center h-[75vh]">
 		<div
 			class="container mx-auto max-w-5xl text-center flex flex-col gap-3 px-4 md:px-0 items-center"
 		>
